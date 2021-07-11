e2f66854fd107d106c0b3aceb21dddb0a89afa7e
Mojahid213 <mojoffi133@gmail.com>
2021-07-11 16:14:51 +0600

    feedback section added

--- src/components/Home/Feedback/Feedback.js
+++ src/components/Home/Feedback/Feedback.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Feedback = () => {
     return (
-        <div className="mt-10">
-            <h2>Mojahid</h2>
+        <div className="mt-16">
+            <div className="text-center">
+                <h1 className="text-4xl font-Signika uppercase font-extrabold">Clients <span className="text-indigo-900">Feedback</span></h1>
+                <p className="font-Ubuntu uppercase">Valuable feedback given by our clients</p>
+            </div>
         </div>
     );
 };
