ba19cf3a38d03c0487dd388a7639ec76feddb4d0
Mojahid213 <mojoffi133@gmail.com>
2021-08-12 18:14:42 +0600

    logout added

--- src/shared/Navbar.js
+++ src/shared/Navbar.js
@@ -41,7 +41,11 @@ const Navbar = () => {
             setScrolled()
         };
     }, [])
-
+    // Handle logout
+    const logout = () => {
+        sessionStorage.clear();
+        window.location.href = '/';
+    }
     return (
         <div className={scrolled ? "px-10 sm:px-16 lg:px-24 py-4 bg-cs-black sticky top-0 z-50 transition duration-500 ease-in-out" : "px-10 sm:px-16 lg:px-24 py-4 sticky top-0 z-50"}>
             <nav className="block md:flex items-center">
@@ -59,13 +63,13 @@ const Navbar = () => {
                     <li className=" mt-4 md:mt-0"><a href="/#">services</a></li>
                     <li className=" mt-4 md:mt-0"><a href="/#">contact</a></li>
                     {JSON.parse(sessionStorage.getItem('token')) ?
-                        <li className="mt-4 md:mt-0 relative">
-                            <button id="profile_pic" className={scrolled ? "h-10 w-10 bg-white text-black text-base font-RobotoSlab rounded-full p-1.5 border-2 border-gray-400 hover:border-opacity-0 ring-indigo-400 hover:ring-4 transition duration-300" : "h-10 w-10 bg-black text-white text-base font-RobotoSlab rounded-full p-1.5 border-2 border-gray-400 hover:border-opacity-0 ring-indigo-400 hover:ring-4 transition duration-300"}>{user.avatar}</button>
+                        <li className="mt-4 md:mt-0 relative text-center">
+                            <button id="profile_pic" className={scrolled ? "flex justify-center h-10 w-10 bg-white text-black text-base font-RobotoSlab rounded-full p-1.5 mx-auto border-2 border-gray-400 hover:border-opacity-0 ring-indigo-400 hover:ring-4 transition duration-300" : "flex justify-center h-10 w-10 bg-black text-white text-base font-RobotoSlab rounded-full p-1.5 mx-auto border-2 border-gray-400 hover:border-opacity-0 ring-indigo-400 hover:ring-4 transition duration-300"}>{user.avatar}</button>
                             <ul id="profile_card" className="hidden absolute right-1/2 md:right-0 -mr-16 md:-mr-0 mt-3 bg-white border shadow-lg rounded-lg text-black p-3 w-52 md:w-60">
-                                <div className="cursor-pointer flex justify-center items-center h-16 w-16 md:h-20 md:w-20 bg-black text-white font-RobotoSlab rounded-full p-1.5 mx-auto"><span className="text-4xl md:text-5xl">{user.avatar}</span></div>
+                                <div className="cursor-pointer flex justify-center items-center h-16 w-16 md:h-20 md:w-20 bg-black text-white font-RobotoSlab rounded-full p-1.5 mx-auto"><span className="text-3xl md:text-4xl">{user.avatar}</span></div>
                                 <li><h1 className="text-lg font-bold font-RobotoSlab text-center">{user.name}</h1></li>
                                 <li><button className="px-4 py-2 mt-4 bg-cs-black text-white text-lg rounded-lg focus:ring-4 ring-gray-600 hover:bg-cs-ebony transition duration-300">Dashboard</button></li>
-                                <li className="mt-4 cursor-pointer text-left"><p className="normal-case hover:text-blue-700"><FontAwesomeIcon icon={faSignOutAlt} /> Logout</p></li>
+                                <li className="mt-4 cursor-pointer text-left"><p onClick={logout} className="normal-case hover:text-blue-700"><FontAwesomeIcon icon={faSignOutAlt} /> Logout</p></li>
                             </ul>
                         </li>
                         : <li className="mt-4 md:mt-0"><Link to="/login"><button className={scrolled ? "bg-white px-5 py-2 text-black hover:text-white rounded-md hover:bg-indigo-400 transition duration-300 ease-in-out" : "bg-black px-5 py-2 text-white rounded-md hover:bg-cs-ebony transition duration-300 ease-in-out"}>LOGIN</button></Link></li>
