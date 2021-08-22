f7daceae563a3068f01f36058a2ba1902093fcc8
Mojahid213 <mojoffi133@gmail.com>
2021-08-22 16:53:59 +0600

    service-component-edited

--- src/components/Home/Services/Services/Services.js
+++ src/components/Home/Services/Services/Services.js
@@ -41,7 +41,7 @@ const Services = () => {
                 <h1 className="text-4xl font-Signika font-extrabold">OUR <span className="font-extrabold text-indigo-900">SERVICES</span></h1>
                 <p className="font-Ubuntu text-lg">Services that we provide in our website</p>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 items-center text-center gap-7 md:gap-10 px-12 sm:px-20 lg:px-32">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 items-center text-center gap-7 md:gap-10 px-8 sm:px-20 lg:px-32">
                 {
                     data.map(detail => <ServiceInfo details={detail} key={detail.id}></ServiceInfo>)
                 }
