a1c1c66ebc1fc72b72cee76e7d26a20ae26ce992
Mojahid213 <mojoffi133@gmail.com>
2021-08-25 18:26:58 +0600

    scalaton loader added

--- src/components/Home/Services/Services/Services.js
+++ src/components/Home/Services/Services/Services.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ServiceInfo from '../ServiceInfo/ServiceInfo';
 import axios from 'axios';
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import Loader from "react-loader-spinner";
 
 
 const Services = () => {
@@ -16,36 +14,38 @@ const Services = () => {
             })
     }, [])
 
-    if (Loading) {
-        return (
-            <div className="mt-12">
-                <div className="text-center mb-10">
-                    <h1 className="text-4xl font-Signika font-extrabold">OUR <span className="font-extrabold text-indigo-900">SERVICES</span></h1>
-                    <p className="font-Ubuntu text-lg">Services that we provide in our website</p>
-                </div>
-                <div className="flex justify-center">
-                    <Loader
-                        type="ThreeDots"
-                        color="#000000"
-                        height={100}
-                        width={100}
-                    />
-                </div>
-            </div>
-        )
-    }
-
     return (
         <div className="mt-12">
             <div className="text-center mb-10">
                 <h1 className="text-4xl font-Signika font-extrabold">OUR <span className="font-extrabold text-indigo-900">SERVICES</span></h1>
                 <p className="font-Ubuntu text-lg">Services that we provide in our website</p>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 items-center text-center gap-7 md:gap-10 px-10 sm:px-20 lg:px-32">
-                {
-                    data.map(detail => <ServiceInfo details={detail} key={detail.id}></ServiceInfo>)
-                }
-            </div>
+            {Loading ?
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 items-center text-center gap-7 md:gap-10 px-10 sm:px-20 lg:px-32">
+                    {
+                        [1, 2, 3].map((detail) =>
+                            <div key={detail} className="flex flex-1 flex-col border-2 border-gray-700 px-2 py-2 h-full shadow-lg transform sm:transform-gpu hover:scale-105 transition duration-500 ease-in-out cursor-pointer">
+                                <div className="h-full animate-pulse">
+                                    <div className="rounded-md mx-auto h-60 bg-gray-400 mb-2"></div>
+                                    <div className="rounded-md h-12 bg-gray-400 mx-auto"></div>
+                                    <div className="flex flex-col gap-y-1 my-2">
+                                        <div className="rounded-md h-4 bg-gray-400"></div>
+                                        <div className="rounded-md h-4 bg-gray-400 w-5/6"></div>
+                                        <div className="rounded-md h-4 bg-gray-400 w-11/12"></div>
+                                        <div className="rounded-md h-4 bg-gray-400"></div>
+                                    </div>
+                                    <div className="rounded-md h-10 bg-gray-400 w-8/12"></div>
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+                : <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 items-center text-center gap-7 md:gap-10 px-10 sm:px-20 lg:px-32">
+                    {
+                        data.map(detail => <ServiceInfo details={detail} key={detail.id}></ServiceInfo>)
+                    }
+                </div>
+            }
         </div>
     );
 };
